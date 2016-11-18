6ddde0826f7f4b18a1c0e5b06c49a7cf220d7244
HyominNam <skagbghk@gmail.com>
2016-11-18 13:11:42 +0900

    Main Javafx

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -3,13 +3,14 @@ package GUI;
 import FileIO.FileIO;
 import Music.CurrentMusic;
 
-import javax.swing.*;
+import javafx.scene.control.MenuBar;;
+import javafx.scene.layout.BorderPane;;
+import javafx.embed.swing.*;
 
-import java.awt.*;
 
 class Main {
-    private final JFrame mainFrame = new JFrame();
-
+    private final BorderPane mainPane = new BorderPane ();
+       
     public static void main(String[] args) {
         Main main = new Main();
         FileIO.makeDirectory(System.getProperty("user.home") + "/Desktop/" + "music-info");
@@ -25,18 +26,15 @@ class Main {
         tabPanel.connectPanels(playerPanel);                            // Connect Panels
 
         CurrentMusic.getInstance().setPlayerTab(playerPanel);           // SetUp Panels
-        this.mainFrame.setLayout(new BorderLayout());                                           //Setting Layout and add Panels start
-        this.mainFrame.setSize(800, 500);
-
-        this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        this.mainFrame.add(tabPanel.createTab(musicList), BorderLayout.WEST);
-        this.mainFrame.add(new JScrollPane(musicList.getPanel()), BorderLayout.CENTER);
-        this.mainFrame.add(playerPanel, BorderLayout.EAST);
-
-        this.mainFrame.setJMenuBar(new Toolbar(musicList));
-        this.mainFrame.getJMenuBar().add(new Toolbar());
         
-        this.mainFrame.setVisible(true);                                                        //Setting Layout and add Panels end
+        this.mainPane.setPrefSize(800, 500);
+        
+        this.mainPane.setTop(new Toolbar(musicList));
+//        addStackPane(hbox); 
+        this.mainPane.setLeft(tabPanel.createTab(musicList));
+        this.mainPane.setCenter(new ScrollBar(musicList.getPanel()));
+        this.mainPane.setRight(playerPanel);
+
+        this.mainPane.setVisible(true);                                                        //Setting Layout and add Panels end
     }
 }
