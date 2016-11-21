e69fdb937a1698f27b4e2cd91ed296a71e040cf9
HyominNam <skagbghk@gmail.com>
2016-11-22 02:01:56 +0900

    Lyric 클래스 코드 추가 및 수정

--- src/main/java/Music/Lyric.java
+++ src/main/java/Music/Lyric.java
@@ -16,4 +16,25 @@ class Lyric {
     }
 
 
+    public String lyricShow(int min, int sec, int msec) {
+        int j = 1, start = 0;
+        while (lrc[j] != null) {    //텍스트 파일의 총 줄수
+            j++;
+        }
+        //출력 알고리즘
+        for (int i = 0; i < j; i++) {
+            if (timeCal(min, sec, msec) >= timeCal(time[i][0], time[i][1], time[i][2])) {
+                start = i;
+            }
+        }
+
+        for (int k = 0; k < start; k++) {
+            if (timeCal(time[start][0], time[start][1], time[start][2]) == timeCal(time[k][0], time[k][1], time[k][2]))
+                start = k;
+            break;
+        }
+
+        return lrc[start];
+
+    }
 }
